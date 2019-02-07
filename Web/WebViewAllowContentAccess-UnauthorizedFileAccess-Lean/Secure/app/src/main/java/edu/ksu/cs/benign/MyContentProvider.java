c4f51ba839bdccc809dec1b4fb14a3b9300db0fd
Aditya Narkar <avnarkar@ksu.edu>
2019-02-07 16:23:38 -0600

    Removed UnsupportedOperationException

--- Web/WebViewAllowContentAccess-UnauthorizedFileAccess-Lean/Secure/app/src/main/java/edu/ksu/cs/benign/MyContentProvider.java
+++ Web/WebViewAllowContentAccess-UnauthorizedFileAccess-Lean/Secure/app/src/main/java/edu/ksu/cs/benign/MyContentProvider.java
@@ -34,20 +34,20 @@ public class MyContentProvider extends ContentProvider {
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         // Implement this to handle requests to delete one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return 0;
     }
 
     @Override
     public String getType(Uri uri) {
         // TODO: Implement this to handle requests for the MIME type of the data
         // at the given URI.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
         // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
 
     @Override
@@ -60,13 +60,13 @@ public class MyContentProvider extends ContentProvider {
     public Cursor query(Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
         // TODO: Implement this to handle query requests from clients.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
         // TODO: Implement this to handle requests to update one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return 0;
     }
 }
