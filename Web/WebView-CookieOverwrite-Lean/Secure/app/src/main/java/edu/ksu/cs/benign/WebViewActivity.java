37b0ccc00fced81ae2a2b6d0fbd93890f90f3ad3
Aditya Narkar <avnarkar@ksu.edu>
2019-02-19 17:17:57 -0600

    Added -> Secure

---
+++ Web/WebView-CookieOverwrite-Lean/Secure/app/src/main/java/edu/ksu/cs/benign/WebViewActivity.java
@@ -0,0 +1,30 @@
+package edu.ksu.cs.benign;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview_activity);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Ben", "In webview");
+        final CookieManager cookieManager = CookieManager.getInstance();
+        final WebView webView = findViewById(R.id.WebView);
+        final WebSettings webSettings = webView.getSettings();
+
+        cookieManager.setAcceptCookie(false);
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl(getIntent().getStringExtra("url"));
+    }
+}
