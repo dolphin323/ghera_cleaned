50ee870fdea6cc20145deaecaa143a663c8ca8d5
Aditya Narkar <avnarkar@ksu.edu>
2019-02-24 15:55:19 -0600

    Source scripts --> Deleted empty functions

--- Web/WebViewLoadDataWithBaseUrl-UnauthorizedFileAccess-Lean/source-script.ps1
+++ Web/WebViewLoadDataWithBaseUrl-UnauthorizedFileAccess-Lean/source-script.ps1
@@ -7,14 +7,6 @@ function before_test_vuln() {
   & $ANDROID_HOME\platform-tools\adb.exe push File2 /data/data/edu.ksu.cs.benign/files/File2
 }
 
-function after_uninstall_vuln() {
-  #put any task that needs to be performed after uninstalling vulnerable
-}
-
-function before_install_secure() {
-  #put any task that needs to be performed before installing secure
-}
-
 function before_test_secure() {
   before_test_vuln
 }
