8f4ac598657e85b5af351793c3852cbe21888f2e
Aditya Narkar <avnarkar@ksu.edu>
2019-02-27 17:20:48 -0600

    http server modified

--- Misc/LocalServer/index_http.py
+++ Misc/LocalServer/index_http.py
@@ -1,13 +1,33 @@
 from flask import render_template
 from flask import Flask
 from flask import request
+from flask_httpauth import HTTPBasicAuth
+
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
     if func is None:
         raise RuntimeError('Not running serever')
     func()
+
+users = {
+    "john": "hello",
+    "susan": "bye"
+}
+
+@auth.get_password
+def get_pw(username):
+    print(username)
+    if username in users:
+        return users.get(username)
+    return None
+
+@app.route('/<ssn>')
+@auth.login_required
+def secureEntry(ssn):
+    return "Hello, %s! SSN=%s" % (auth.username(), ssn)
 
 @app.route('/test/')
 @app.route('/test/<name>')
