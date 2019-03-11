c98fea54fc3445f2653b6d69add9bcc1d068499d
Aditya Narkar <avnarkar@ksu.edu>
2019-03-11 18:47:21 -0500

    source-scripts

--- Web/WebViewProceed-UnauthorizedAccess-Lean/source-script.ps1
+++ Web/WebViewProceed-UnauthorizedAccess-Lean/source-script.ps1
@@ -2,18 +2,10 @@ function before_install_vuln() {
   $webServer = Start-Process powershell "cd ..\..\Misc\LocalServer; python.exe .\index_http_webviewproceed_unauthorizedaccess.py" -PassThru
 }
 
-function after_uninstall_vuln() {
-  # after_uninstall_secure
-}
-
-function before_install_secure() {
-  # before_install_vuln
-}
-
 function after_uninstall_secure() {
   $web = New-Object Net.WebClient
   [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
-  $web.DownloadString("http://localhost:5000/shutdown")
+  $web.DownloadString("https://localhost:5000/shutdown")
   [System.Net.ServicePointManager]::ServerCertificateValidationCallback = $null
   echo "killed the web server"
 }
