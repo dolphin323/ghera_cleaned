165bd7f88fbe0e77ba18dffb274acb7d932b624a
Aditya Narkar <avnarkar@ksu.edu>
2019-03-18 18:49:29 -0500

    Updated the source-scripts

--- Web/WebViewProceed-UnauthorizedAccess-Lean/source-script.sh
+++ Web/WebViewProceed-UnauthorizedAccess-Lean/source-script.sh
@@ -1,6 +1,6 @@
 function before_install_vuln() {
   cd ../../Misc/LocalServer/
-  python index_http_webviewproceed_unauthorizedaccess.py &
+  python index_https_webviewproceed_unauthorizedaccess.py &
   echo "benchmark path = $1"
   cd $1
 }
